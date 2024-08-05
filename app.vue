485d39da8b7c8864c5aee103979121559795f2b5
XiYang6666 <1782356858@qq.com>
2024-08-05 17:39:05 +0800

    :tada: initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="bg h-dvh w-dvw overflow-hidden bg-black bg-opacity-80 flex items-center justify-center flex-col"
+  >
+    <span class="text-zinc-400 text-4xl">XAS Minecraft Server</span>
+    <span class="text-zinc-400 text-2xl m-4">给我玩 MC 😡</span>
+    <span
+      class="footer fixed bottom-4 text-lg text-zinc-400 font-thin text-center"
+      ><a href="https://beian.miit.gov.cn/" target="_blank"
+        >陕ICP备2023002983号-2</a
+      ></span
+    >
+  </div>
+</template>
+
+<script lang="ts" setup></script>
